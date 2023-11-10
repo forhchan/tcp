bbad62701a2c0dfebca5277b4b9c0fe18151be99
forhchan <cyclonely1911@gmail.com>
2023-11-10 11:25:38 +0900

    2023-11-10

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 import socket
 
 # HOST = '192.168.93.128'
-HOST = '192.168.93.128'
-PORT = 3333
+HOST = '167.179.103.201'
+PORT = 8000
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))    
+print(f"Connected to {HOST}")
 while True:
     message = input("Leave a message : ")
     sock.send(f"{message}".encode('utf-8'))
